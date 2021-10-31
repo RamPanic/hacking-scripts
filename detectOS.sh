13183ba5d5a9b403fc46277837dd249fe5852e1d
ram <dementor20@protonmail.com>
2021-10-30 23:21:47 -0300

    Detect OS Script comments

--- detectOS.sh
+++ detectOS.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Este método puede no ser exacto todo el tiempo. 
+# Sin embargo, dará una idea del sistema operativo 
+# subyacente en un sistema remoto.
+
 HOST=$1
 
 ttl=$(ping -c 1 $HOST | egrep -o "ttl=[0-9]+" | cut -d "=" -f2)
